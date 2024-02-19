e52046192a2b27ff01abdc3084c9d5d35fe691ca
Stephen Karanja <karanjastephenn@gmail.com>
2024-02-19 16:56:48 +0300

    chaining after catch in async await

---
+++ js/promises/using_promises/13-chainingAfterCatch.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// In async/await
+async function main() {
+  try {
+    await doSomething();
+    throw new Error("Something failed");
+    console.log("Do this");
+  } catch (e) {
+    console.error("Do that");
+  }
+  console.log("Do this, no matter what happened before");
+}
