166171da7915371e474db398d22040ff07bc3a70
Stephen Karanja <karanjastephenn@gmail.com>
2024-02-19 13:34:24 +0300

    handling errors

---
+++ js/promises/using_promises/9-exception.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/* This symmetry with asynchronous code culminates in the async/await syntax: */
+
+async function foo() {
+  try {
+    const result = await doSomething();
+    const newResult = await doSomethingElse(result);
+    const finalResult = await doThirdThing(newResult);
+    console.log(`Got the final result: ${finalResult}`);
+  } catch (error) {
+    failureCallback(error);
+  }
+}
