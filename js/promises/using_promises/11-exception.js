19f9abdfb5b7713db237734f16ef10937a39b5a8
Stephen Karanja <karanjastephenn@gmail.com>
2024-02-19 13:48:26 +0300

    catch scope in async/await

---
+++ js/promises/using_promises/11-exception.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/* catch scope in async/await */
+async function main() {
+  try {
+    const result = await doSomethingCritical();
+    try {
+      const optionalResult = await doSomethingOptional(result);
+      await doSomethingExtraNice(optionalResult);
+    } catch (e) {
+      // Ignore failures in optional steps and proceed.
+    }
+    await moreCriticalStuff();
+  } catch (e) {
+    console.error(`Critical failure: ${e.message}`);
+  }
+}
