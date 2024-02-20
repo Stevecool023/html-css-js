57f4b7105763fab98a87d8d628e691fd713889a1
Stephen Karanja <karanjastephenn@gmail.com>
2024-02-20 17:16:24 +0300

    Thenables

---
+++ js/promises/promise-object/4-thenable.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const aThenable = {
+  then(onFulfilled, onRejected) {
+    onFulfilled({
+      // The thenable is fulfilled with another thenable
+      then(onFulfilled, onRejected) {
+        onFulfilled(42);
+      },
+    });
+  },
+};
+
+Promise.resolve(aThenable); // A promise fulfilled with 42
